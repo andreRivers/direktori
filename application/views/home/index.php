504605d52e56a31200c51f76751ecf7c7681eab8
PowerRanger <triandre04@gmail.com>
2021-04-17 14:20:02 +0700

    Startup

---
+++ application/views/home/index.php
@@ -0,0 +1,46 @@
+
+  <div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image: url('assets/assets/img/bg2.jpg');">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 ml-auto mr-auto">
+
+
+        <div class="brand">
+        <h1>Direktori UMSU</h1>
+		<form action="/action_page.php">
+		<div class="row">
+			<div class="col-lg-5">
+				 <input type="text" class="form-control" id="keyword" placeholder="Kata Kunci" name="keyword" style="color: white;" required>
+				</div>
+				
+			<div class="col-lg-5">
+			<select class="form-control" id="sel1" name="sellist1" style="color: white;" required>
+      		<option selected disabled value="" style="color: black;">Pilih Direktori</option>
+       		<option value="dirdosen" style="color: black;">Direktori Dosen</option>
+        	<option value="dirtendik" style="color: black;">Direktori Tendik</option>
+        	<option value="dirmahasiswa" style="color: black;">Direktori Mahasiswa</option>
+      		</select> 
+			</div>
+
+			<div class="col-lg-2">
+				<button type="submit" class="btn btn-primary mt-3"><i class="fa fa-search"></i> Cari</button>
+			</div>
+
+		</div>
+  </form>
+		</div>
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="main main-raised">
+	  <div class="section section-basic">
+
+	  </div>
+  </div>
+ 
---
+++ application/views/layout/header.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets/'); ?>assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="<?= base_url('assets/'); ?>assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+   <?= $title; ?>
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="<?= base_url('assets/'); ?>assets/css/material-kit.css?v=2.0.7" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="<?= base_url('assets/'); ?>assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="index-page sidebar-collapse">
+  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="<?= base_url('home'); ?>"> <img src="<?= base_url('assets/img/'); ?>logo.png" style="width:10%;"> DIREKTORI BAUM</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+			
+		<li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
+              <i class="fa fa-youtube"></i>
+            </a>
+          </li>
+      
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
+              <i class="fa fa-twitter"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com/" target="_blank" data-original-title="Like us on Facebook" rel="nofollow">
+              <i class="fa fa-facebook-square"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/" target="_blank" data-original-title="Follow us on Instagram" rel="nofollow">
+              <i class="fa fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>

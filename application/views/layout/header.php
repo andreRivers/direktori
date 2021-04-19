f2a90516544ad0ed94b32b87f395585dcddb1d16
PowerRanger <triandre04@gmail.com>
2021-04-19 14:44:22 +0700

    ganti template

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,62 +1,66 @@
-
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets/'); ?>assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="<?= base_url('assets/'); ?>assets/img/favicon.png">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>
-   <?= $title; ?>
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- CSS Files -->
-  <link href="<?= base_url('assets/'); ?>assets/css/material-kit.css?v=2.0.7" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="<?= base_url('assets/'); ?>assets/demo/demo.css" rel="stylesheet" />
-</head>
-
-<body class="index-page sidebar-collapse">
-  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
-    <div class="container">
-      <div class="navbar-translate">
-        <a class="navbar-brand" href="<?= base_url('home'); ?>"> <img src="<?= base_url('assets/img/'); ?>logo.png" style="width:10%;"> DIREKTORI BAUM</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="navbar-toggler-icon"></span>
-          <span class="navbar-toggler-icon"></span>
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav ml-auto">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?= $title; ?></title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="<?= base_url('assets/'); ?>css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-dark text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">DIREKTORI UMSU</a>
+                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+              
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead bg-info text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+			<img class="masthead-avatar mb-5" src="<?= base_url('assets/'); ?>/logo.png" alt="" />
+			<h2 class="masthead-subheading font-weight-light mb-0">Selamat Datang di</h2>
+			<br>
+                <h1 class="masthead-heading text-uppercase mb-0">DIREKTORI UMSU</h1>
+			<br>
 			
-		<li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
-              <i class="fa fa-youtube"></i>
-            </a>
-          </li>
-      
-          <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
-              <i class="fa fa-twitter"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com/" target="_blank" data-original-title="Like us on Facebook" rel="nofollow">
-              <i class="fa fa-facebook-square"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/" target="_blank" data-original-title="Follow us on Instagram" rel="nofollow">
-              <i class="fa fa-instagram"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+			</div>
+			<form>
+			<div class="container">
+			<div class="control-group">
+				<div class="row">
+					<div class="col-xl-5">
+						<input type="text" class="form-control form-control-lg mb-2" id="keyword" placeholder="Kata Kunci" name="keyword" required>
+					</div>
+			
+				
+					<div class="col-xl-5">
+					<select class="form-control form-control-lg mb-2" id="kategori" name="kategori" required>
+				     	<option selected disabled value="">Silahkan Pilih</option>
+						<option value="dosen">Dosen</option>
+						<option value="tendik">Tendik</option>
+						<option value="mahasiswa">Mahasiswa</option>
+					</select>
+					</div>
+
+					<div class="col-xl-2">
+						<button class="btn btn-danger form-control-lg" type="submit"><i class="fa fa-search"></i> Search</button>
+					</div>
+				</div>
+				</div>
+			</div>
+		</form>
+        </header>
